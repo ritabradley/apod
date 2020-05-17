6dc86fed1a2e97caa73dcc068c0bdfd97a77f877
Rita Bradley <contact@ritabradley.com>
2020-05-17 10:09:38 -0700

    styles: Add project styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import NasaPhoto from '../components/NasaPhoto';
+import './App.css';
 
 function App() {
     return (
